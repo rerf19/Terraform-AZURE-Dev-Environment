939c5e9c85e652e716221616b1248094ca25710e
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2022-12-13 15:00:46 +0000

    subnet and security rules

--- main.tf
+++ main.tf
@@ -32,6 +32,24 @@ resource "azurerm_virtual_network" "mtc-vn" {
   }
 }
 
+#state (!IMPORTANT!) - We have terraform.tfstate that has all the resources created
+#                    - We also have terraform.tfstate.backup if you we destroy by mistake, just rename it.
 
+#subnet
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
 
+#security rules
+resource "azurerm_network_security_rule" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
 
+  tags = {
+    enviroment = "dev"
+  }
+}
