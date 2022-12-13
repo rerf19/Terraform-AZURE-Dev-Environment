82decf648a1be7b241058b395c17bc7e4d79c1da
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2022-12-13 10:23:33 +0000

    user group and virtual network created

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.91.0"
+      version = "2.97.0"
     }
   }
 }
@@ -10,4 +10,28 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+#create a user group - these need to be created to group all my enviroment inside 
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "ukwest"
+  tags = {
+    enviroment = "dev"
+  }
+}
+
+#virtual network
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name #referecing is important
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    enviroment = "dev"
+  }
+}
+
+
+
 
