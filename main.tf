5647a633def9511cd61e2f455e49f10460db4d00
Rodrigo Ferreira <r.reisferreira@lancaster.ac.uk>
2022-12-23 02:26:01 +0000

    public ip and network interface

--- main.tf
+++ main.tf
@@ -71,6 +71,35 @@ resource "azurerm_network_security_rule" "mtc-dev-rule" {
 
 #associate out security group with our sub-net
 resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
-  subnet_id                          = azurerm_subnet.mtc-subnet.id
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
   network_security_group_id = azurerm_network_security_group.mtc-sg.id
+}
+
+#create public ip
+resource "azurerm_public_ip" "mtc-ip" {
+  name                = "mtc-ip"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    enviroment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "mtc_nic" {
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
+  }
+
+  tags = {
+    enviroment = "dev"
+  }
 }
