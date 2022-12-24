2be873bc19d4fcdf8fab4f20d76ee35265619d85
Rodrigo Ferreira <rodrigo.e09@hotmail.com>
2022-12-24 17:34:57 +0000

    create vm

--- main.tf
+++ main.tf
@@ -103,3 +103,32 @@ resource "azurerm_network_interface" "mtc_nic" {
     enviroment = "dev"
   }
 }
+
+#ssh-keygen -t rsa
+#C:\Users\Rodrigo/.ssh/mtcazurekey
+
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                  = "mtc-vm"
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  location              = azurerm_resource_group.mtc-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.mtc_nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/mtcazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntoServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
